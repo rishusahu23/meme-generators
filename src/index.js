787f605bcfe979d86fe9de8076ffa1568ac21060
Rishu Sahu <rishusahu23@gmail.con>
2020-04-20 20:48:07 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import React from 'react';
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<h1>hello</h1>,document.getElementById('root'))
+
+
+
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -15,3 +23,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
