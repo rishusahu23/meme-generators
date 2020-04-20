d9c497a0135988a4b57db07b54009eedd39e91ce
Rishu Sahu <rishusahu23@gmail.con>
2020-04-20 22:26:08 +0530

    MYinfo.js

---
+++ src/MyInfo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MyInfo=()=>{
+	return (
+		<div>
+			<h1>rishu sahu</h1>
+			<p>bech it</p>
+			<ul>
+				<li>1</li>
+				<li>2</li>
+				<li>3</li>
+			</ul>
+		</div>
+		)
+}
+export default MyInfo
