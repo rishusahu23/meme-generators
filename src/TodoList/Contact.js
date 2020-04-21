57342393014c2f49712469ed53fdddeaef7d7409
Rishu Sahu <rishusahu23@gmail.con>
2020-04-21 12:06:18 +0530

    all committed

---
+++ src/TodoList/Contact.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Contact=()=>{
+	return (
+		<div className="flex-container">
+			<div className="contact-item">
+				<img src="https://robohash.org/rishu" alt="hello"/>
+				<h3>Rishu Sahu</h3>
+				<p>9125654485</p>
+				<p>rishusahu23@gmail.com</p>
+			</div>
+			<div className="contact-item">
+				<img src="https://robohash.org/rishu" alt="hello"/>
+				<h3>Rishu Sahu</h3>
+				<p>9125654485</p>
+				<p>rishusahu23@gmail.com</p>
+			</div>
+			<div className="contact-item">
+				<img src="https://robohash.org/rishu" alt="hello"/>
+				<h3>Rishu Sahu</h3>
+				<p>9125654485</p>
+				<p>rishusahu23@gmail.com</p>
+			</div>
+			<div className="contact-item">
+				<img src="https://robohash.org/rishu" alt="hello"/>
+				<h3>Rishu Sahu</h3>
+				<p>9125654485</p>
+				<p>rishusahu23@gmail.com</p>
+			</div>
+			<div className="contact-item">
+				<img src="https://robohash.org/rishu" alt="hello"/>
+				<h3>Rishu Sahu</h3>
+				<p>9125654485</p>
+				<p>rishusahu23@gmail.com</p>
+			</div>
+			<div className="contact-item">
+				<img src="https://robohash.org/rishu" alt="hello"/>
+				<h3>Rishu Sahu</h3>
+				<p>9125654485</p>
+				<p>rishusahu23@gmail.com</p>
+			</div>
+			<div className="contact-item">
+				<img src="https://robohash.org/rishu" alt="hello"/>
+				<h3>Rishu Sahu</h3>
+				<p>9125654485</p>
+				<p>rishusahu23@gmail.com</p>
+			</div>
+			<div className="contact-item">
+				<img src="https://robohash.org/rishu" alt="hello"/>
+				<h3>Rishu Sahu</h3>
+				<p>9125654485</p>
+				<p>rishusahu23@gmail.com</p>
+			</div>
+		</div>
+		)
+}
+
+export default Contact;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+//import App from './App'
+import Contact from './TodoList/Contact'
 
 
 
-ReactDOM.render(<App />,document.getElementById('root'))
+ReactDOM.render(<Contact />,document.getElementById('root'))
 
 
 
